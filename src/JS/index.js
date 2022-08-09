ae9d0b24327facf2e0e33271aad7edabf0709c60
Avinash Gupta <73595465+tier3guy@users.noreply.github.com>
2022-08-09 19:28:14 +0530

    Revert "Fixed #100 #111 #112."

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,6 +1,7 @@
 let wrapper = document.querySelector('.wrapper');
 let loader = document.querySelector('.loading');
 let bounceBall = document.querySelector('.bounceball');
+let bounceBall_style = window.getComputedStyle(bounceBall,'::before');
 let content = document.querySelector('.content');
 
 
@@ -10,12 +11,14 @@ window.addEventListener('load', function(){
     this.setTimeout(function(){
         wrapper.style.display = 'none';
         document.querySelector('.main-content').style.display = 'block';
-    },2000)
+    },3000)
 })
 document.querySelector('.theme-toggle-button').addEventListener('click',() =>{
+    console.log("click")
     document.body.classList.toggle('dark')
 })
 document.querySelector('.theme-toggle-button2').addEventListener('click',() =>{
+    console.log("click")
     document.body.classList.toggle('dark')
 })
 
