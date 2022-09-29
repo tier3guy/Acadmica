ac3f2174ccaf3398de48ee709f70c5b3bd48ba2e
Aritra Mondal <sasuke.aritra.sa@gmail.com>
2022-09-29 23:30:10 +0530

    search bar functionality added in interview page

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,17 +1,29 @@
-window.addEventListener('scroll',(e)=>{
+window.addEventListener('scroll', (e) => {
   const nav = document.getElementById('nav')
-  if(window.pageYOffset>0){
+  if (window.pageYOffset > 0) {
     nav.classList.add("add-shadow");
-  }else{
+  } else {
     nav.classList.remove("add-shadow");
   }
 });
 
-document.querySelector('.theme-toggle-button').addEventListener('click',() =>{
+document.querySelector('.theme-toggle-button').addEventListener('click', () => {
   document.body.classList.toggle('dark');
   document.querySelector('.theme-icon').classList.toggle("fa-moon");
 })
 
-document.querySelector('.theme-toggle-button2').addEventListener('click',() =>{
+document.querySelector('.theme-toggle-button2').addEventListener('click', () => {
   document.body.classList.toggle('dark')
+})
+
+const searchbar = document.getElementById("search-item")
+
+
+searchbar.childNodes[5].addEventListener('click', (event) => {
+  event.preventDefault();
+  var searchTerm = searchbar.childNodes[3].value
+  window.find(searchTerm)
+  if (!window.find(searchTerm)) {
+    alert('Sorry not available')
+  }
 })
